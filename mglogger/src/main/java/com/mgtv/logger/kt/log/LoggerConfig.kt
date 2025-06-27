c1e8244363c8268a6ea0f3aed8009ba3243b4969
CountOfMonteCristo <37135857+blantian@users.noreply.github.com>
2025-06-27 22:57:35 +0800

    feat(logger): add builder setters

--- mglogger/src/main/java/com/mgtv/logger/kt/log/LoggerConfig.kt
+++ mglogger/src/main/java/com/mgtv/logger/kt/log/LoggerConfig.kt
@@ -25,6 +25,14 @@ data class LoggerConfig(
         var maxQueue: Int = 10_000
         var key16: String = "1234567890abcdef"
         var iv16: String = "abcdef1234567890"
+        fun setCachePath(path: String): Builder = apply { cachePath = path }
+        fun setLogDir(dir: String): Builder = apply { logDir = dir }
+        fun setKeepDays(days: Long): Builder = apply { keepDays = days }
+        fun setMaxFile(value: Long): Builder = apply { maxFile = value }
+        fun setMinSdCard(value: Long): Builder = apply { minSdCard = value }
+        fun setMaxQueue(value: Int): Builder = apply { maxQueue = value }
+        fun setKey16(key: String): Builder = apply { this.key16 = key }
+        fun setIv16(iv: String): Builder = apply { this.iv16 = iv }
         fun build() =
             LoggerConfig(cachePath, logDir, keepDays, maxFile, minSdCard, maxQueue, key16, iv16)
     }
