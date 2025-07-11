e076b1162439c756a54914355082f63c24c19896
lantian <lantian@mgtv.com>
2025-07-11 15:16:46 +0800

    添加队列

---
+++ mglogger/src/main/cpp/mglogger/mg/message_queue.cpp
@@ -0,0 +1,9 @@
+/**
+ * Description:
+ * Created by lantian 
+ * Date： 2025/7/11
+ * Time： 15:16
+ */
+
+
+#include "message_queue.h"
---
+++ mglogger/src/main/cpp/mglogger/mg/message_queue.h
@@ -0,0 +1,18 @@
+/**
+ * Description:
+ * Created by lantian 
+ * Date： 2025/7/11
+ * Time： 15:16
+ */
+
+
+#ifndef MGLOGGER_MESSAGE_QUEUE_H
+#define MGLOGGER_MESSAGE_QUEUE_H
+
+
+class message_queue {
+
+};
+
+
+#endif //MGLOGGER_MESSAGE_QUEUE_H
